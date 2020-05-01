4795019c45162177cb901b51a02ebe9e6553b693
Sam Blissard <samblissard@gmail.com>
2020-04-30 22:25:41 -0400

    Update entity import

--- src/coffee/coffee.service.ts
+++ src/coffee/coffee.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Coffee } from 'src/entities/coffee.entity';
+import { Coffee } from 'src/database/entities/coffee.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
