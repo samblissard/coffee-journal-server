4ecfd82d2db330d5919b95e2ff3b5185bc9ec16b
Sam Blissard <samblissard@gmail.com>
2020-05-01 20:48:45 -0400

    Mapping coffee entity to DTO

--- src/coffee/coffee.controller.ts
+++ src/coffee/coffee.controller.ts
@@ -1,14 +1,17 @@
 import { Controller, Get, Body, Post } from '@nestjs/common';
 import { CoffeeService } from './coffee.service';
 import { Coffee } from '../database/entities/coffee.entity';
+import { mapCoffeeToCoffeePreviewDTO } from './mappers/coffee-mappers';
+import { CoffeePreviewDTO } from './dto/coffee-preview-dto';
 
 @Controller('coffee')
 export class CoffeeController {
   constructor(private readonly coffeeService: CoffeeService) {}
 
   @Get()
-  async findAll(): Promise<Coffee[]> {
-    return await this.coffeeService.findAll();
+  async findAll(): Promise<CoffeePreviewDTO[]> {
+    const coffeeEntities = await this.coffeeService.findAll();
+    return coffeeEntities.map(coffee => mapCoffeeToCoffeePreviewDTO(coffee));
   }
 
   @Post()
