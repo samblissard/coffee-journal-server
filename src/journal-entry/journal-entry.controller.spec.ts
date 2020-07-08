8799e8897d8fa1f4bb5a61872be608d0469d3dfe
Sam Blissard <samblissard@gmail.com>
2020-07-07 20:36:20 -0400

    Test for journal entry controller create

--- src/journal-entry/journal-entry.controller.spec.ts
+++ src/journal-entry/journal-entry.controller.spec.ts
@@ -3,15 +3,23 @@ import { JournalEntryController } from './journal-entry.controller';
 import { JournalEntryService } from './journal-entry.service';
 import { Repository } from 'typeorm';
 import { CoffeeService } from '../coffee/coffee.service';
+import { JournalEntryDTO } from './dto/journal-entry-dto';
+import { JournalEntry } from '../database/entities/journal-entry.entity';
 
 describe('JournalEntry Controller', () => {
   let controller: JournalEntryController;
+  let mockJournalEntryService: JournalEntryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [JournalEntryController],
       providers: [
-        JournalEntryService,
+        {
+          provide: JournalEntryService,
+          useValue: {
+            create: jest.fn(),
+          },
+        },
         CoffeeService,
         {
           provide: 'JournalEntryRepository',
@@ -25,9 +33,32 @@ describe('JournalEntry Controller', () => {
     }).compile();
 
     controller = module.get<JournalEntryController>(JournalEntryController);
+    mockJournalEntryService = module.get<JournalEntryService>(
+      JournalEntryService,
+    );
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should call JournalEntryService create with mapped entity', async () => {
+      const dto: JournalEntryDTO = {
+        coffee: {
+          id: 0,
+          name: 'someName',
+          roast: 'someRoast',
+          roaster: 'someRoaster',
+          tastingNotes: [],
+        },
+      };
+
+      await controller.create(dto);
+
+      expect(mockJournalEntryService.create).toHaveBeenCalledWith(
+        jasmine.any(JournalEntry),
+      );
+    });
   });
 });
