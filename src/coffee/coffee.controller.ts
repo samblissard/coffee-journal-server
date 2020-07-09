da07de40ac870dc72acde0c953d174f5f60fe0d3
Sam Blissard <samblissard@gmail.com>
2020-07-08 22:46:38 -0400

    Adding swagger annotations for controllers and DTOs

--- src/coffee/coffee.controller.ts
+++ src/coffee/coffee.controller.ts
@@ -1,22 +1,32 @@
 import { Controller, Get, Body, Post } from '@nestjs/common';
 import { CoffeeService } from './coffee.service';
-import { Coffee } from '../database/entities/coffee.entity';
 import {
   mapCoffeeToCoffeeDTO,
   mapCoffeeDTOToCoffee,
 } from './mappers/coffee-mappers';
 import { CoffeeDTO } from './dto/coffee-dto';
+import {
+  ApiTags,
+  ApiResponse,
+  ApiOperation,
+  ApiProperty,
+} from '@nestjs/swagger';
 
+@ApiTags('coffee')
 @Controller('coffee')
 export class CoffeeController {
   constructor(private readonly coffeeService: CoffeeService) {}
 
+  @ApiOperation({ summary: 'Get all coffee' })
   @Get()
   async findAll(): Promise<CoffeeDTO[]> {
     const coffeeEntities = await this.coffeeService.findAll();
     return coffeeEntities.map(coffee => mapCoffeeToCoffeeDTO(coffee));
   }
 
+  @ApiOperation({ summary: 'Create coffee' })
+  @ApiResponse({ status: 201, description: 'Created' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
   @Post()
   async create(@Body() coffee: CoffeeDTO) {
     const coffeeEntity = mapCoffeeDTOToCoffee(coffee);
--- src/journal-entry/journal-entry.controller.ts
+++ src/journal-entry/journal-entry.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { JournalEntryService } from './journal-entry.service';
-import { JournalEntry } from '../database/entities/journal-entry.entity';
 import { JournalEntryDTO } from './dto/journal-entry-dto';
 import { mapJournalEntryDTOToJournalEntry } from './mappers/journal-entry-mapper';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('journal-entry')
 @Controller('journal-entry')
 export class JournalEntryController {
   constructor(private readonly journalEntryService: JournalEntryService) {}
 
+  @ApiOperation({ summary: 'Create journal entry' })
+  @ApiResponse({ status: 201, description: 'Created' })
+  @ApiResponse({ status: 400, description: 'Bad request' })
   @Post()
   async create(@Body() journalEntryDTO: JournalEntryDTO) {
     const journalEntry = mapJournalEntryDTOToJournalEntry(journalEntryDTO);
